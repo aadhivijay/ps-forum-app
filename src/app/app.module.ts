dd20337c8c034ed5eaf0c8c48c1217a958bda703
Aadhivijay <ravjduker@gmail.com>
2017-08-03 15:56:44 +0530

    tables created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,33 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
+import { HttpModule } from '@angular/http';
+import { RouterModule } from '@angular/router';
+
 import { BlueColorDirective } from './blue-color.directive';
+import { HomeComponent } from './home/home.component';
+import { CreatepostComponent } from './createpost/createpost.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    BlueColorDirective
+    BlueColorDirective,
+    HomeComponent,
+    CreatepostComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpModule,
+    RouterModule.forRoot([
+      {
+        path: 'home',
+        component: HomeComponent
+      },
+      {
+        path: 'createpost',
+        component: CreatepostComponent
+      }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
